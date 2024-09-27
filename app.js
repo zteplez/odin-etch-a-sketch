368f2e7e064659d73f333f440d37d5d66d00b15d
zteplez <asibrahim98@gmail.com>
2024-09-27 12:01:10 +0300

    [fix]: Fix grid overflow

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
+const changeSizeButton = document.querySelector("button");
+const drawGrid = document.querySelector(".container .draw-grid");
+const gridDimension = 960;
+let gridSize = 16; // Default value.
+
 function initGrid(){
-    for(let i = 0; i < 16 * 16; i++){
+    let boxSize = gridDimension / gridSize;
+
+    for(let i = 0; i < gridSize * gridSize; i++){
             let div = document.createElement("div");
             div.classList.add("grid-box");
-            document.querySelector(".container").append(div);
+            div.style.width = boxSize + "px";
+            div.style.height = boxSize + "px";
+            document.querySelector(".draw-grid").append(div);
     }
 }
+function clearGrid(){
+    let grids = document.querySelectorAll(".grid-box");  
+    grids.forEach(element => {
+        element.remove();
+    });
+}
+
+changeSizeButton.addEventListener('click', () => {
+    gridSize = prompt("Enter new size (Max:100)");
+    if(gridSize > 80 || gridSize < 16){
+        alert("Invalid number!");
+        return;
+    }
+    clearGrid();
+    initGrid();
+});
 
 initGrid();
